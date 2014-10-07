0a370ecd7ec938a7dba09893691251f4bbcc3eb7
mitjanikolaus <mitjanikolaus@gmail.com>
2014-10-07 12:28:29 +0200

    reduced logging of testbed adapter

--- testbed/src/main/java/org/fiteagle/adapters/testbed/dm/InformListenerMDB.java
+++ testbed/src/main/java/org/fiteagle/adapters/testbed/dm/InformListenerMDB.java
@@ -1,20 +1,28 @@
 package org.fiteagle.adapters.testbed.dm;
 
-import com.hp.hpl.jena.rdf.model.*;
-import com.hp.hpl.jena.vocabulary.RDF;
-import com.hp.hpl.jena.vocabulary.RDFS;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.Topic;
+
 import org.fiteagle.adapters.testbed.OntologyReader;
 import org.fiteagle.api.core.IMessageBus;
 import org.fiteagle.api.core.MessageBusMsgFactory;
 import org.fiteagle.api.core.MessageBusOntologyModel;
 
-import javax.annotation.Resource;
-import javax.ejb.ActivationConfigProperty;
-import javax.ejb.MessageDriven;
-import javax.inject.Inject;
-import javax.jms.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
 
 /**
  * Created by vju on 9/28/14.
@@ -28,27 +36,24 @@ public class InformListenerMDB implements MessageListener {
 
     @Inject
     private JMSContext context;
-    @Resource(mappedName = IMessageBus.TOPIC_CORE_NAME)
+    @javax.annotation.Resource(mappedName = IMessageBus.TOPIC_CORE_NAME)
     private Topic topic;
 
     public void onMessage(final Message requestMessage) {
         try {
 
-            LOGGER.log(Level.INFO, "GOT INFORM MESSAGE IN TESTBEDADAPTER");
             if (requestMessage.getStringProperty(IMessageBus.METHOD_TYPE) != null) {
 
                 Model modelMessage = MessageBusMsgFactory.getMessageRDFModel(requestMessage);
                 if (modelMessage != null && isAdapterMessage(modelMessage)) {
-                    LOGGER.log(Level.INFO, "GOT INFORM MESSAGE IN TESTBEDADAPTER + XXXXXX");
-
                     if (requestMessage.getStringProperty(IMessageBus.METHOD_TYPE).equals(IMessageBus.TYPE_INFORM)) {
                         Model adapterModel = OntologyReader.getTestbedModel();
-                        com.hp.hpl.jena.rdf.model.Resource adapterInstance = getAdapterInstance(modelMessage);
+                        Resource adapterInstance = getAdapterInstance(modelMessage);
                         if (adapterInstance == null) {
                             LOGGER.log(Level.INFO, "http://fiteagle.org/ontology#Adapter could not be detected");
                         } else {                            
-                            // LOGGER.log(Level.INFO, "GOT INFORM MESSAGE IN TESTBEDADAPTER");
-                            com.hp.hpl.jena.rdf.model.Resource testbedResource = adapterModel.getResource("http://fiteagleinternal#FITEAGLE_Testbed");
+                             LOGGER.log(Level.INFO, "GOT INFORM MESSAGE IN TESTBEDADAPTER");
+                            Resource testbedResource = adapterModel.getResource("http://fiteagleinternal#FITEAGLE_Testbed");
                             testbedResource.addProperty(MessageBusOntologyModel.propertyFiteagleContainsAdapter, adapterInstance);
                             sendUpdateModel(adapterModel);
                         }
