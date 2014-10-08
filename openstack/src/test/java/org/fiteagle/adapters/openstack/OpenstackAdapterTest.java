31ece3b5e7641b0e531bf05db99931e356dbf97d
mitjanikolaus <mitjanikolaus@gmail.com>
2014-10-08 17:29:15 +0200

    extended openstackadaptertest

--- openstack/src/test/java/org/fiteagle/adapters/openstack/OpenstackAdapterTest.java
+++ openstack/src/test/java/org/fiteagle/adapters/openstack/OpenstackAdapterTest.java
@@ -1,24 +1,43 @@
 package org.fiteagle.adapters.openstack;
 
 import org.fiteagle.api.core.MessageBusOntologyModel;
+
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.OWL;
+import com.hp.hpl.jena.vocabulary.RDF;
 import com.hp.hpl.jena.vocabulary.RDFS;
 
 public class OpenstackAdapterTest {
   
   private OpenstackAdapter openstackAdapter = OpenstackAdapter.getInstance();
   
+  Model adapterModel = openstackAdapter.getAdapterDescriptionModel();
+  private Resource adapterType = openstackAdapter.getAdapterType();
+  private Resource adapterInstanceType = openstackAdapter.getAdapterInstance();
+  private Resource resourceType = openstackAdapter.getAdapterManagedResource();
+  
   @Test
   public void testAdapterModel() {
-    Model adapterModel = openstackAdapter.getAdapterDescriptionModel();
-    Resource adapterType = openstackAdapter.getAdapterType();
-    Resource resourceType = openstackAdapter.getAdapterManagedResource();
     assertTrue(adapterModel.contains(adapterType, RDFS.subClassOf, MessageBusOntologyModel.classAdapter));
+    assertTrue(adapterModel.contains(adapterType, RDF.type, OWL.Class)); 
+  }
+  
+  @Test
+  public void testAdapterResourceModel() {
     assertTrue(adapterModel.contains(resourceType, RDFS.subClassOf, MessageBusOntologyModel.classResource)); 
+    assertTrue(adapterModel.contains(resourceType, MessageBusOntologyModel.propertyFiteagleImplementedBy, adapterType)); 
+    assertTrue(adapterModel.contains(resourceType, RDF.type, OWL.Class)); 
+  }
+  
+  @Test
+  public void testAdapterInstanceModel() {
+    assertTrue(adapterModel.contains(adapterInstanceType, RDF.type, adapterType)); 
+    assertTrue(adapterModel.contains(adapterInstanceType, RDF.type, adapterType)); 
   }
   
 }
