c8d171f1a4643095165d981bca27938d78ac65ad
mitjanikolaus <mitjanikolaus@gmail.com>
2014-10-08 19:14:49 +0200

    cleanup

--- mightyrobot/src/main/java/org/fiteagle/adapters/mightyrobot/MightyRobotAdapterContextListener.java
+++ mightyrobot/src/main/java/org/fiteagle/adapters/mightyrobot/MightyRobotAdapterContextListener.java
@@ -1,8 +1,6 @@
 package org.fiteagle.adapters.mightyrobot;
 
 
-import javax.jms.JMSException;
-import javax.servlet.ServletContextEvent;
 import javax.servlet.annotation.WebListener;
 
 import org.fiteagle.abstractAdapter.AbstractAdapter;
@@ -11,24 +9,6 @@ import org.fiteagle.abstractAdapter.AbstractAdapterContextListener;
 @WebListener
 public class MightyRobotAdapterContextListener extends AbstractAdapterContextListener {
 
-  @Override
-  public void contextInitialized(ServletContextEvent event) {
-    System.out.println(this.getClass().getSimpleName() + ": Registering adapter " + getAdapter());
-    
-    getAdapter().registerAdapter();
-    
-    // At this point maybe some parameters of the adapter itself should be restored as well?!
-    // getAdapter().restoreAdapterParameters();
-    
-    try {
-      restoreState();
-    } catch (JMSException e) {
-//      LOGGER.log(Level.SEVERE, e.getMessage());
-    }
-  }
-  
-  
-  
   @Override
   protected AbstractAdapter getAdapter() {
     return MightyRobotAdapter.getInstance();
