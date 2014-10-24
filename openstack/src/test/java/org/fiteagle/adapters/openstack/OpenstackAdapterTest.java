b2bcb957f5128ba93728debf4fc3f57a573435d5
mitjanikolaus <mitjanikolaus@gmail.com>
2014-10-24 16:10:23 +0200

    Revert "updated some openstack client dependencies"
    This reverts commit 2730a9d020f5315a797476911040aec296d842d2.

--- openstack/src/test/java/org/fiteagle/adapters/openstack/OpenstackAdapterTest.java
+++ openstack/src/test/java/org/fiteagle/adapters/openstack/OpenstackAdapterTest.java
@@ -1,7 +1,6 @@
 package org.fiteagle.adapters.openstack;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.Iterator;
 
