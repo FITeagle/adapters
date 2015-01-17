44b1af660a2c5c0c555b6926e022ee47f8673399
mitjanikolaus <mitjanikolaus@gmail.com>
2015-01-17 15:21:52 +0100

    fixed ttl file to create openstack VM

--- openstack/src/main/java/org/fiteagle/adapters/openstack/client/OpenstackParser.java
+++ openstack/src/main/java/org/fiteagle/adapters/openstack/client/OpenstackParser.java
@@ -174,7 +174,7 @@ public class OpenstackParser {
     Resource imagesResource = adapterModel.createResource(adapter.getAdapterInstance().getURI()+"_images");
     int i = 1;
     for(Image image : images.getList()){
-      Resource imageResource = adapterModel.createResource(adapter.getAdapterInstance().getNameSpace()+image.getName().replace(" ", "_"));
+      Resource imageResource = adapterModel.createResource(adapter.getAdapterInstance().getNameSpace()+image.getName().replace(" ", "_").replace(".","-"));
       imageResource.addProperty(RDF.type, adapter.getImageResource());
       imageResource.addProperty(PROPERTY_IMAGE_ID, adapterModel.createLiteral(image.getId()));
       imageResource.addProperty(RDFS.label, adapterModel.createLiteral(image.getName()));
