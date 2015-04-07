b2293d76817895444369da08e11c93c1f44af965
Alex-O. <wpinf05.ortlieb@gmail.com>
2015-04-07 19:51:43 +0200

    Fixed addUser and some other bugs

--- sshService/src/main/java/org/fiteagle/adapters/sshService/dm/SshServiceAdapterREST.java
+++ sshService/src/main/java/org/fiteagle/adapters/sshService/dm/SshServiceAdapterREST.java
@@ -3,17 +3,24 @@ package org.fiteagle.adapters.sshService.dm;
 import java.util.Map;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response.Status;
 
+import org.apache.jena.atlas.logging.Log;
 import org.fiteagle.abstractAdapter.AbstractAdapter;
 import org.fiteagle.abstractAdapter.dm.AbstractAdapterREST;
+import org.fiteagle.abstractAdapter.dm.AbstractAdapterREST.AdapterWebApplicationException;
 import org.fiteagle.adapters.sshService.SshServiceAdapter;
+import org.fiteagle.api.core.OntologyModelUtil;
 
 @Path("/")
 public class SshServiceAdapterREST extends AbstractAdapterREST {
   
   @Override
   protected Map<String, AbstractAdapter> getAdapterInstances() {
+	  Log.fatal("Instances", SshServiceAdapter.adapterInstances.toString());
     return SshServiceAdapter.adapterInstances;
   }
+  
+  
   
 }
