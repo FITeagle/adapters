0d2ad0300c8d8b7fbe045d4ece444fd0f1c9c948
andisadewi <andisa.dewi@gmail.com>
2015-04-27 00:55:34 +0700

    added VM URI

--- monitoring/monitoringSQLresource.py
+++ monitoring/monitoringSQLresource.py
@@ -82,11 +82,11 @@ logger.debug("Connecting to SQLite...")
 with con :
 		try :
 			cur = con.cursor()
-			cur.execute("select distinct(host_name), collector_uri from virtual_physical_map")
+			cur.execute("select distinct(host_name), collector_uri, vm_uri from virtual_physical_map")
 			rows = cur.fetchall()
 			logger.debug("Fetching all host names from database...")
 			for row in rows :
-				print row[0], row[1]
+				print row[0], row[1], row[2]
 		except:
 			logger.error("Error fetching data from SQLite.")
 			sys.exit()
@@ -96,12 +96,19 @@ if not rows :
 	sys.exit()
 
 #-----------------------------------------------------------------------#
-omlInst = oml4py.OMLBase(monitoring_settings["appname"],monitoring_settings["domain"],monitoring_settings["sender"], "tcp:"+row[1])
+omlInst = oml4py.OMLBase(monitoring_settings["appname"],monitoring_settings["domain"],monitoring_settings["sender"], "tcp:130.149.22.139")
+#omlInst = oml4py.OMLBase(monitoring_settings["appname"],monitoring_settings["domain"],monitoring_settings["sender"], "tcp:" + row[1])
+#omlInst.addmp("availability", "availability:int32:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:Availability}{MOFID:Availability|MOFID:hasMetricAttributes|MOFID:AvailabilityMeasurement}{MOFID:AvailabilityMeasurement|MOFID:MeasurementDataValue|%value%} timestamp:datetime:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:Availability}{MOFID:Availability|MOFID:hasMetricAttributes|MOFID:AvailabilityMeasurement}{MOFID:AvailabilityMeasurement|MOFID:timestamp|%value%} hostname:string:{omn:Resource|omn-resource:hasName|%value%} ")
+#omlInst.addmp("total_memory", "total_memory:double:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:TotalMemory}{MOFID:TotalMemory|MOFID:hasMetricAttributes|MOFID:TotalMemoryMeasurement}{MOFID:TotalMemoryMeasurement|MOFID:MeasurementDataValue|%value%}{MOFID:TotalMemoryMeasurement|MOFID:isMeasuredIn|MOFIU:Byte}{MOFID:TotalMemoryMeasurement|MOFIU:hasPrefix|MOFIU:giga} timestamp:datetime:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:TotalMemory}{MOFID:TotalMemory|MOFID:hasMetricAttributes|MOFID:TotalMemoryMeasurement}{MOFID:TotalMemoryMeasurement|MOFID:timestamp|%value%} hostname:string:{omn:Resource|omn-resource:hasName|%value%} ")
+#omlInst.addmp("available_memory", "available_memory:double:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:AvailableMemory}{MOFID:AvailableMemory|MOFID:hasMetricAttributes|MOFID:AvailableMemoryMeasurement}{MOFID:AvailableMemoryMeasurement|MOFID:MeasurementDataValue|%value%}{MOFID:AvailableMemoryMeasurement|MOFID:isMeasuredIn|MOFIU:Byte}{MOFID:AvailableMemoryMeasurement|MOFIU:hasPrefix|MOFIU:giga} timestamp:datetime:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:AvailableMemory}{MOFID:AvailableMemory|MOFID:hasMetricAttributes|MOFID:AvailableMemoryMeasurement}{MOFID:AvailableMemoryMeasurement|MOFID:timestamp|%value%} hostname:string:{omn:Resource|omn-resource:hasName|%value%} ")
+#omlInst.addmp("used_memory", "used_memory:double:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:UsedMemory}{MOFID:UsedMemory|MOFID:hasMetricAttributes|MOFID:UsedMemoryMeasurement}{MOFID:UsedMemoryMeasurement|MOFID:MeasurementDataValue|%value%}{MOFID:UsedMemoryMeasurement|MOFID:isMeasuredIn|MOFIU:Byte}{MOFID:UsedMemoryMeasurement|MOFIU:hasPrefix|MOFIU:giga} timestamp:datetime:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:UsedMemory}{MOFID:UsedMemory|MOFID:hasMetricAttributes|MOFID:UsedMemoryMeasurement}{MOFID:UsedMemoryMeasurement|MOFID:timestamp|%value%} hostname:string:{omn:Resource|omn-resource:hasName|%value%} ")
 
-omlInst.addmp("availability", "hostname:string:{omn:Resource|omn-resource:hasName|%value%} availability:int32:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:Availability}{MOFID:Availability|MOFID:hasMetricAttributes|MOFID:AvailabilityMeasurement}{MOFID:AvailabilityMeasurement|MOFID:MeasurementDataValue|%value%} timestamp:datetime:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:Availability}{MOFID:Availability|MOFID:hasMetricAttributes|MOFID:AvailabilityMeasurement}{MOFID:AvailabilityMeasurement|MOFID:timestamp|%value%} ")
-omlInst.addmp("total_memory", "hostname:string:{omn:Resource|omn-resource:hasName|%value%} total_memory:double:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:TotalMemory}{MOFID:TotalMemory|MOFID:hasMetricAttributes|MOFID:TotalMemoryMeasurement}{MOFID:TotalMemoryMeasurement|MOFID:MeasurementDataValue|%value%}{MOFID:TotalMemoryMeasurement|MOFID:isMeasuredIn|MOFIU:Byte}{MOFID:TotalMemoryMeasurement|MOFIU:hasPrefix|MOFIU:giga} timestamp:datetime:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:TotalMemory}{MOFID:TotalMemory|MOFID:hasMetricAttributes|MOFID:TotalMemoryMeasurement}{MOFID:TotalMemoryMeasurement|MOFID:timestamp|%value%} ")
-omlInst.addmp("available_memory", "hostname:string:{omn:Resource|omn-resource:hasName|%value%} available_memory:double:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:AvailableMemory}{MOFID:AvailableMemory|MOFID:hasMetricAttributes|MOFID:AvailableMemoryMeasurement}{MOFID:AvailableMemoryMeasurement|MOFID:MeasurementDataValue|%value%}{MOFID:AvailableMemoryMeasurement|MOFID:isMeasuredIn|MOFIU:Byte}{MOFID:AvailableMemoryMeasurement|MOFIU:hasPrefix|MOFIU:giga} timestamp:datetime:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:AvailableMemory}{MOFID:AvailableMemory|MOFID:hasMetricAttributes|MOFID:AvailableMemoryMeasurement}{MOFID:AvailableMemoryMeasurement|MOFID:timestamp|%value%} ")
-omlInst.addmp("used_memory", "hostname:string:{omn:Resource|omn-resource:hasName|%value%} used_memory:double:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:UsedMemory}{MOFID:UsedMemory|MOFID:hasMetricAttributes|MOFID:UsedMemoryMeasurement}{MOFID:UsedMemoryMeasurement|MOFID:MeasurementDataValue|%value%}{MOFID:UsedMemoryMeasurement|MOFID:isMeasuredIn|MOFIU:Byte}{MOFID:UsedMemoryMeasurement|MOFIU:hasPrefix|MOFIU:giga} timestamp:datetime:{omn:Resource|MOFID:hasMeasurementMetric|MOFID:UsedMemory}{MOFID:UsedMemory|MOFID:hasMetricAttributes|MOFID:UsedMemoryMeasurement}{MOFID:UsedMemoryMeasurement|MOFID:timestamp|%value%} ")
+#omlInst.addmp("total_memory", "total_memory:double:{MOFID:Measurement|MOFID:isMeasurementOf|MOFID:TotalMemory}{MOFID:TotalMemory|MOFID:isMeasurementMetricOf|omn:Resource}{MOFID:TotalMemory|MOFID:hasMetricAttributes|MOFID:TotalMemoryMeasurement}{MOFID:TotalMemoryMeasurement|MOFID:MeasurementDataValue|%value%}{MOFID:TotalMemoryMeasurement|MOFID:isMeasuredIn|MOFIU:Byte}{MOFID:TotalMemoryMeasurement|MOFIU:hasPrefix|MOFIU:giga} timestamp:datetime:{MOFID:Measurement|MOFID:isMeasurementOf|MOFID:TotalMemory}{MOFID:TotalMemory|MOFID:isMeasurementMetricOf|omn:Resource}{MOFID:TotalMemory|MOFID:hasMetricAttributes|MOFID:TotalMemoryMeasurement}{MOFID:TotalMemoryMeasurement|MOFID:timestamp|%value%} physicalresource:string:{omn-domain-pc:PC|rdfs:label|%value%} virtualresource:string:{omn-domain-pc:VM|rdfs:label|%value%} ")
+#omlInst.addmp("available_memory", "available_memory:double:{MOFID:Measurement|MOFID:isMeasurementOf|MOFID:AvailableMemory}{MOFID:AvailableMemory|MOFID:isMeasurementMetricOf|omn:Resource}{MOFID:AvailableMemory|MOFID:hasMetricAttributes|MOFID:AvailableMemoryMeasurement}{MOFID:AvailableMemoryMeasurement|MOFID:MeasurementDataValue|%value%}{MOFID:AvailableMemoryMeasurement|MOFID:isMeasuredIn|MOFIU:Byte}{MOFID:AvailableMemoryMeasurement|MOFIU:hasPrefix|MOFIU:giga} timestamp:datetime:{MOFID:Measurement|MOFID:isMeasurementOf|MOFID:AvailableMemory}{MOFID:AvailableMemory|MOFID:isMeasurementMetricOf|omn:Resource}{MOFID:AvailableMemory|MOFID:hasMetricAttributes|MOFID:AvailableMemoryMeasurement}{MOFID:AvailableMemoryMeasurement|MOFID:timestamp|%value%} physicalresource:string:{omn-domain-pc:PC|rdfs:label|%value%} virtualresource:string:{omn-domain-pc:VM|rdfs:label|%value%} ")
+#omlInst.addmp("used_memory", "used_memory:double:{MOFID:Measurement|MOFID:isMeasurementOf|MOFID:UsedMemory}{MOFID:UsedMemory|MOFID:isMeasurementMetricOf|omn-domain-pc:PC}{MOFID:UsedMemory|MOFID:hasMetricAttributes|MOFID:UsedMemoryMeasurement}{MOFID:UsedMemoryMeasurement|MOFID:MeasurementDataValue|%value%}{MOFID:UsedMemoryMeasurement|MOFID:isMeasuredIn|MOFIU:Byte}{MOFID:UsedMemoryMeasurement|MOFIU:hasPrefix|MOFIU:giga} timestamp:datetime:{MOFID:Measurement|MOFID:isMeasurementOf|MOFID:UsedMemory}{MOFID:UsedMemory|MOFID:isMeasurementMetricOf|omn-domain-pc:PC}{MOFID:UsedMemory|MOFID:hasMetricAttributes|MOFID:UsedMemoryMeasurement}{MOFID:UsedMemoryMeasurement|MOFID:timestamp|%value%} physicalresource:string:{omn-domain-pc:PC|rdfs:label|%value%} virtualresource:string:{omn-domain-pc:VM|rdfs:label|%value%} ")
+#omlInst.addmp("availability", "availability:int32:{MOFID:Measurement|MOFID:isMeasurementOf|MOFID:Availability}{MOFID:Availability|MOFID:isMeasurementMetricOf|omn:Resource}{MOFID:Availability|MOFID:hasMetricAttributes|MOFID:AvailabilityMeasurement}{MOFID:AvailabilityMeasurement|MOFID:MeasurementDataValue|%value%} timestamp:datetime:{MOFID:Measurement|MOFID:isMeasurementOf|MOFID:Availability}{MOFID:Availability|MOFID:isMeasurementMetricOf|omn:Resource}{MOFID:Availability|MOFID:hasMetricAttributes|MOFID:AvailabilityMeasurement}{MOFID:AvailabilityMeasurement|MOFID:timestamp|%value%} physicalresource:string:{omn-domain-pc:PC|rdfs:label|%value%} virtualresource:string:{omn-domain-pc:VM|rdfs:label|%value%} ")
+omlInst.addmp("used_bandwidth", "used_bandwidth:double:{MOFID:Measurement|MOFID:isMeasurementOf|MOFID:UsedBandwidth}{MOFID:UsedBandwidth|MOFID:isMeasurementMetricOf|omn-resource:Interface}{omn-resource:Interface|omn-resource:isInterfaceOf|omn-domain-pc:PC}{MOFID:UsedBandwidth|MOFID:hasMetricAttributes|MOFID:UsedBandwidthMeasurement}{MOFID:UsedBandwidthMeasurement|MOFID:MeasurementDataValue|%value%}{MOFID:UsedBandwidthMeasurement|MOFIU:hasPrefix|MOFIU:mega}{MOFID:UsedBandwidthMeasurement|MOFID:defaultUnit|MOFID:bitspersec} timestamp:datetime:{MOFID:Measurement|MOFID:isMeasurementOf|MOFID:UsedBandwidth}{MOFID:UsedBandwidth|MOFID:isMeasurementMetricOf|omn-resource:Interface}{omn-resource:Interface|omn-resource:isInterfaceOf|omn-domain-pc:PC}{MOFID:UsedBandwidth|MOFID:hasMetricAttributes|MOFID:UsedBandwidthMeasurement}{MOFID:UsedBandwidthMeasurement|MOFID:timestamp|%value%} physicalresource:string:{omn-domain-pc:PC|rdfs:label|%value%} virtualresource:string:{omn-domain-pc:VM|rdfs:label|%value%} ")
+
 #-----------------------------------------------------------------------#
 omlInst.start()
 
@@ -129,15 +136,21 @@ for row in rows :
 		totalmem = float(zapi.item.get({"output": "extend","hostids":hostid,"search":{"name":"Total memory"}}).pop()['lastvalue']) / (1024)**3
 		usedmem = float(zapi.item.get({"output": "extend","hostids":hostid,"search":{"name":"Used memory"}}).pop()['lastvalue']) / (1024)**3
 		availmem = float(zapi.item.get({"output": "extend","hostids":hostid,"search":{"name":"Available memory"}}).pop()['lastvalue']) / (1024)**3
-
+		usedbandwidth = float(zapi.item.get({"output": "extend","hostids":hostid,"search":{"key_":"net.if.in[eth2]"}}).pop()['lastvalue']) / (1024)**2
+	
 	except Exception as e:
 		print e
 		logger.error("cannot fetch data from Zabbix.")
 		sys.exit()
 
-	omlInst.inject("total_memory", [row[0], totalmem, current])
-	omlInst.inject("used_memory", [row[0], usedmem, current])
-	omlInst.inject("available_memory", [row[0], availmem, current])
+	#omlInst.inject("total_memory", [row[0], totalmem, current])
+	#omlInst.inject("used_memory", [row[0], usedmem, current])
+	#omlInst.inject("available_memory", [row[0], availmem, current])
+
+	#omlInst.inject("total_memory", [totalmem, current, row[0], row[2]])
+	#omlInst.inject("used_memory", [usedmem, current, row[0], row[2]])
+	#omlInst.inject("available_memory", [availmem, current, row[0], row[2]])
+	omlInst.inject("used_bandwidth", [usedbandwidth, current, row[0], row[2]])
 
 omlInst.close()		
 
