73e6b7de0dbd0d3de1236916984dca0cc1f920db
dnehls <daniel.nehls@tu-berlin.de>
2015-07-06 12:38:06 +0200

    tosca adapter refactoring

--- tosca/src/main/java/org/fiteagle/adapters/tosca/dm/ToscaAdapterREST.java
+++ tosca/src/main/java/org/fiteagle/adapters/tosca/dm/ToscaAdapterREST.java
@@ -2,18 +2,23 @@ package org.fiteagle.adapters.tosca.dm;
 
 import java.util.Collection;
 
+import javax.ejb.EJB;
 import javax.ws.rs.Path;
 
 import org.fiteagle.abstractAdapter.AbstractAdapter;
 import org.fiteagle.abstractAdapter.dm.AbstractAdapterREST;
 import org.fiteagle.adapters.tosca.ToscaAdapter;
+import org.fiteagle.adapters.tosca.ToscaAdapterControl;
 
 @Path("/")
 public class ToscaAdapterREST extends AbstractAdapterREST {
-  
+
+
+  @EJB
+  ToscaAdapterControl adapterControl;
   @Override
   protected Collection<AbstractAdapter> getAdapterInstances() {
-    return ToscaAdapter.adapterInstances;
+    return adapterControl.getAdapterInstances();
   }
   
 }
--- tosca/src/main/java/org/fiteagle/adapters/tosca/dm/ToscaMDBSender.java
+++ tosca/src/main/java/org/fiteagle/adapters/tosca/dm/ToscaMDBSender.java
@@ -1,20 +1,14 @@
 package org.fiteagle.adapters.tosca.dm;
 
-import java.util.Collection;
 
+import javax.ejb.EJB;
 import javax.ejb.Singleton;
-import javax.ejb.Startup;
 
-import org.fiteagle.abstractAdapter.AbstractAdapter;
 import org.fiteagle.abstractAdapter.dm.AbstractAdapterMDBSender;
-import org.fiteagle.adapters.tosca.ToscaAdapter;
+import org.fiteagle.adapters.tosca.ToscaAdapterControl;
 
 @Singleton
-@Startup
 public class ToscaMDBSender extends AbstractAdapterMDBSender {
-  
-  @Override
-  protected Collection<AbstractAdapter> getAdapterInstances() {
-    return ToscaAdapter.adapterInstances;
-  }
+  @EJB
+  ToscaAdapterControl adapterControl;
 }
