8bb36d22182a62212cd42cd04f3945ca34d4652a
Alex-O. <wpinf05.ortlieb@gmail.com>
2015-10-30 16:05:02 +0100

    Starting to implement the adapters functions

--- Open5GCore/src/main/java/org/fiteagle/adapters/Open5GCore/Open5GCoreAdapterControl.java
+++ Open5GCore/src/main/java/org/fiteagle/adapters/Open5GCore/Open5GCoreAdapterControl.java
@@ -7,29 +7,39 @@ import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.inject.Inject;
 
 import org.fiteagle.abstractAdapter.AbstractAdapter;
 import org.fiteagle.abstractAdapter.AdapterControl;
+import org.fiteagle.adapters.Open5GCore.dm.Open5GCoreAdapterMDBSender;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 
 @Singleton
 @Startup
 public class Open5GCoreAdapterControl extends AdapterControl {
+    @Inject
+    protected Open5GCoreAdapterMDBSender mdbSender;
+    
+    
 
     Logger LOGGER = Logger.getLogger(this.getClass().getName());
 
 	@PostConstruct
 	public void init(){
         LOGGER.log(Level.INFO, "Starting Open5GCoreAdapter");
+        this.adapterModel = ModelFactory.createDefaultModel();
+
 
     	this.propertiesName = "Open5GCoreAdapter";
 	}
 	@Override
-	public AbstractAdapter createAdapterInstance(Model model, Resource resource) {
-		// TODO Auto-generated method stub
-		return null;
+	public AbstractAdapter createAdapterInstance(Model tbox, Resource abox) {
+        Open5GCoreAdapter adapter =  new Open5GCoreAdapter(tbox,abox);
+        adapter.setListener(mdbSender);
+        return  adapter;
 	}
 
 	@Override
